fb3700bdede776732db58f018860e99f3186f727
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-06-18 17:08:09 +0200

    fixes #953 extracted Openerp spec settings in spec_helper

--- spec/openerp_spec.rb
+++ spec/openerp_spec.rb
@@ -1,15 +1,39 @@
 require 'spec_helper'
 
 describe Openerp do
-  describe '.login' do
-    it "require dbname, user_name, password" do
-      Openerp.host = 'localhost'
-      Openerp.port = '8069'
-      Openerp.common = '/xmlrpc/common'
+  context 'for common URI' do
 
-      Openerp.login('dbname', 'user', 'password')
+    before :each do
+      openerp_settings
+    end
+
+    describe '.common_client' do
+      it 'build new XMLRPC connection client Proxy' do
+        allow_message_expectations_on_nil
+        XMLRPC::Client.should_receive(:new).with('localhost','/xmlrpc/common','8069').and_return().should_receive(:proxy).with(nil)
+        Openerp.common_client
+      end
+    end
+
+    describe '.login' do
+      it "require dbname, user_name, password" do
+        XMLRPC::Client::Proxy.any_instance.stub(:login).with('dbname','user', 'password')
+        Openerp.login('dbname', 'user', 'password')
+      end
+
+      it "respond with Runtime error when authentication fails" do
+        XMLRPC::Client::Proxy.any_instance.stub(:login).with('dbname','user', 'password').and_return()
+        Openerp.login
+      end
+
+
+
+      it "respons with user id when authentication is successfull"
 
     end
+
   end
+
+
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,10 @@ require 'rubygems'
 require 'bintje'
 
 RSpec.configure do |config|
+end
 
+def openerp_settings
+  Openerp.host = 'localhost'
+  Openerp.port = '8069'
+  Openerp.common = '/xmlrpc/common'
 end
