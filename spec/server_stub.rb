50a2f3898d8d116b0ad4b7a1ee042767e333456f
Cedric Brancourt <cedric.brancourt@gmail.com>
2014-03-21 10:28:19 +0100

    added more comments in server stub

--- spec/server_stub.rb
+++ spec/server_stub.rb
@@ -1,9 +1,11 @@
 module ServerStub
 
+  # Load fake faultCode
   def self.faultCode
     File.read('spec/data/faultCode.txt')
   end
 
+  # Load fake faultString
   def self.faultString
     File.read('spec/data/faultString.txt')
   end
@@ -11,38 +13,37 @@ module ServerStub
   module Common
     module Login
 
-      ##
       # Common to all login stubs
-      #
-
       def self.prologue
         XMLRPC::Client::Proxy.any_instance
         .stub(:login).with('dbname', 'user','password')
       end
 
-      ##
       # faultCode for wrong database name
-      #
       def self.wrong_database_name_fault_code
         %q(FATAL:  database "dbname" does not exist)
       end
 
-      ##
+      
+      # Fake UID for success
+      def self.uid
+        2
+      end
+
+
       # Simulate wrong database name response
-      #
       def self.wrong_data_base_name
         self.prologue
         .and_raise(XMLRPC::FaultException.new(self.wrong_database_name_fault_code, 'bla'))
       end
 
+      # Simulate wrong user name
       def self.wrong_user_name
         self.prologue.and_return(false)
       end
 
-      def self.uid
-        2
-      end
 
+      # Simulate success
       def self.successful
         self.prologue.and_return(self.uid)
       end
@@ -51,24 +52,28 @@ module ServerStub
   end
 
   module Object
+
+    # Embed connection stubbing reused in specific modules
     module Connection
       def self.prologue
-
         XMLRPC::Client.stub(:new).with(OpenObject.host, OpenObject.object, OpenObject.port)
         .and_return().stub(:proxy).with(nil,'dbname',2,'pwd').and_return()
       end
     end
 
     module Search
+      # Initialise stub request with default args
       def self.prologue
         Connection.prologue.stub(:execute)
         .with('receiver_model','search', [['field', 'operator', 'value' ]],0,0)
       end
-
+      
+      # Stub success result
       def self.successful
           self.prologue.and_return([1,2])
       end
 
+      # Stub failure result
       def self.failure
         self.prologue.and_raise(XMLRPC::FaultException.new(ServerStub.faultCode, ServerStub.faultString))
       end
@@ -76,57 +81,69 @@ module ServerStub
     end
 
     module Read
+      # Initialise stub request with default args
       def self.prologue
         Connection.prologue.stub(:execute)
         .with('receiver_model','read',[1,2],['fields'])
       end
-
+      
+      # Standard response shortcut
       def self.standard_response
         [{id:1,name:'un'},{id:2,name:'two'}]
       end
 
+      # Stub success result
       def self.successful
         self.prologue.and_return(self.standard_response)
       end
 
+      # Stub failure result
       def self.failure
         self.prologue.and_raise(XMLRPC::FaultException.new(ServerStub.faultCode, ServerStub.faultString))
       end
     end
 
     module Write
+      # Initialise stub request with default args
       def self.prologue
         Connection.prologue.stub(:execute)
         .with('receiver_model','write',[1,2],{field:'value'})
       end
 
+      # Standard response shortcut
       def self.standard_response
         true
       end
 
+      # Stub success result
       def self.successful
         self.prologue.and_return(self.standard_response)
       end
 
+      # Stub failure result
       def self.failure
         self.prologue.and_raise(XMLRPC::FaultException.new(ServerStub.faultCode, ServerStub.faultString))
       end
     end
 
     module Create
+      # Initialise stub request with default args
       def self.prologue
         Connection.prologue.stub(:execute)
         .with('receiver_model','create',{field:'value'})
       end
 
+      # Standard response shortcut
       def self.standard_response
         1
       end
 
+      # Stub success result
       def self.successful
         self.prologue.and_return(self.standard_response)
       end
 
+      # Stub failure result
       def self.failure
         self.prologue.and_raise(XMLRPC::FaultException.new(ServerStub.faultCode, ServerStub.faultString))
       end
